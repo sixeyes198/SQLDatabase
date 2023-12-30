e1154e14f7cf27437d3b81cf332ee3b1e48e8f88
Sixeyes198 <yossiguadie@icloud.com>
2023-12-30 17:29:41 +0200

    Daily is done

---
+++ DailySqlPuzzle.sql
@@ -0,0 +1,43 @@
+-- CREATE TABLE FirstTab (
+--      id integer, 
+--      name VARCHAR(10)
+-- )
+
+-- INSERT INTO FirstTab VALUES
+-- (5,'Pawan'),
+-- (6,'Sharlee'),
+-- (7,'Krish'),
+-- (NULL,'Avtaar')
+
+-- SELECT * FROM FirstTab
+
+-- CREATE TABLE SecondTab (
+-- 	id integer 
+-- )
+
+-- INSERT INTO SecondTab VALUES
+-- (5),
+-- (NULL)
+
+-- SELECT * FROM SecondTab
+
+--  1. SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL )
+--ANSWER:0
+
+-- 2.
+-- SELECT COUNT(*) 
+--  FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 )
+--ANSWER: 2
+
+--3. 
+-- SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab )
+--ANSWER: 0
+
+--4 
+-- SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL )
+--ANSWER: 2
+
+	
